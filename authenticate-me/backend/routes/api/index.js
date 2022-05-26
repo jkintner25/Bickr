c2ce7383a7ba87565f6f790491f14254f65902aa
jkintner25 <jkintner25@gmail.com>
2022-05-26 18:31:08 -0500

    finished phase 3

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,4 +1,34 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
